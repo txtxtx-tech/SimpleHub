4e8b6fe2d06dbd437673a60cb997536a5c9b62d7
RBXSimpleHub <beautravesuk@gmail.com>
2023-02-07 15:03:04 +0000

    Fixed loader

--- loader.lua
+++ loader.lua
@@ -7,21 +7,23 @@ local safe_request = request or http_request or (http and http.request) or (syn 
 local HttpService = game:GetService("HttpService")
 local repo = "https://api.github.com/repos/RBXSimpleHub/SimpleHub/contents/Source/%s"
 
+safe_request({Url="https://api.github.com/repos/RBXSimpleHub/SimpleHub/contents/Source/", Method = "GET"})
+
 local function parseobject(item)
     if item.type == "dir" then
         local items = HttpService:JSONDecode(
-            safe_request({ Url = item.path:gsub("Source/", ""), Method = "GET", Headers = { ["Content-Type"] = "application/json" } }).Body
+            safe_request({Url=string.format(repo, ({item.path:gsub("Source/", "")})[1]), Method = "GET"}).Body
         )
 
         for _, itemInLoop in next, items do
             parseobject(itemInLoop)
         end
     elseif item.type == "file" then
-        local content = safe_request({ Url = item.download_url, Method = "GET", Headers = { ["Content-Type"] = "application/json" } }).Body
-        writefile(string.format("SimpleHub/%s", item.path:gsub("Source/", "")):gsub(".lua", ""), contents)
+        local content = safe_request({Url=item.download_url, Method = "GET"}).Body
+        writefile("SimpleHub/" .. ({item.path:gsub("Source/", "")})[1], contents)
     end
 end
 
-for _, itemInLoop in next, HttpService:JSONDecode( safe_request({ Url = repo:gsub("/%s", ""), Method = "GET", Headers = { ["Content-Type"] = "application/json" } }).Body ) do
+for _, itemInLoop in next, HttpService:JSONDecode( safe_request({Url="https://api.github.com/repos/RBXSimpleHub/SimpleHub/contents/Source/", Method = "GET"}).Body ) do
     parseobject(itemInLoop)
 end
