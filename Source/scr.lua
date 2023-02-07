8aa755ec342a9683cd0133dd0f6ce4dd4f177fc9
RBXSimpleHub <beautravesuk@gmail.com>
2023-02-07 18:22:12 +0000

    changed loaddependency and where it's stored

--- Source/scr.lua
+++ Source/scr.lua
@@ -2,24 +2,22 @@
     SimpleHub™ - 2023
 ]]
 
--- simple function  for loading dependencies
-local function LoadDependency(name)
-    local DependencyBody = readfile(string.format("SimpleHub/Dependencies/%s.lua", name))
-    local Dependency = loadstring(DependencyBody)
-
-    return Dependency()
-end
-
 -- remove any original simplehub processes that might exist
 local process = getgenv().SimpleHub do
     if process then end
 end
 
 getgenv().SimpleHub = {
-    ["LoadDependency"] = LoadDependency -- shared function
+    ["LoadDependency"] = function(name) -- shared function
+        local DependencyBody = readfile(string.format("SimpleHub/Dependencies/%s.lua", name))
+        local Dependency = loadstring(DependencyBody)
+    
+        return Dependency()
+    end
 }
 
 -- main hub
+local LoadDependency = getgenv().SimpleHub.LoadDependency
 local Promise = LoadDependency("Promise")
 local Webserver = LoadDependency("Webserver")
 local Maid = LoadDependency("Maid")
